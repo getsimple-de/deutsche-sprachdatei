80dd6bf9974d9a8881d62d35c2bf7d5fd9ce408a
Thorsten <thorsten@polyfragmentiert.de>
2011-04-25 13:10:27 +0200

    Erster Commit. Import der aktuellen Datei aus Connies altem Zweig

---
+++ de_DE.php
@@ -0,0 +1,496 @@
+<?php
+/****************************************************
+*
+* @File: 			de_DE.php
+* @Package:			GetSimple
+* @Subject:			DE Deutsche Sprachdatei
+* @Version:			GetSimple 2.0 / GetSimple 3.0
+* @Status:			Final  
+* @Traductors: 			polyfragmented / Connie
+*****************************************************/
+
+$i18n = array(
+
+/* 
+ * For: install.php
+*/
+"PHPVER_ERROR"			=>	"<b>Nicht imstande fortzufahren:</b> PHP 5.1.3 oder h%ouml;her wird ben&ouml;tigt, Sie haben ",
+"SIMPLEXML_ERROR"		=>	"<b>Nicht imstande fortzufahren:</b> <em>SimpleXML</em> ist nicht installiert",
+"CURL_WARNING"			=>	"<b>Warnung:</b> <em>cURL</em> ist nicht installiert",
+"TZ_WARNING"			=>	"<b>Warnung:</b> <em>date_default_timezone_set</em> ist nicht gesetzt",
+"WEBSITENAME_ERROR"		=>	"<b>Fehler:</b> Es gibt ein Problem mit dem Titel Ihrer Website",
+"WEBSITEURL_ERROR"		=>	"<b>Fehler:</b> Es gibt ein Problem mit der URL Ihrer Website",
+"USERNAME_ERROR"		=>	"<b>Fehler:</b> Benutzername war nicht gesetzt",
+"EMAIL_ERROR"			=>	"<b>Fehler:</b> Es gibt ein Problem mit Ihrer E-Mail Adresse",
+"CHMOD_ERROR"			=>	"<b>Nicht imstande fortzufahren:</b> Die Konfigurationsdatei kann nicht geschrieben werden. F%uuml;hren Sie <em>CHMOD 777</em> auf den Ordnern /data/ und /backups/ aus und versuchen Sie es erneut",
+"EMAIL_COMPLETE"		=>	"Installation vollständig",
+"EMAIL_USERNAME"		=>	"Ihr Benutzername lautet",
+"EMAIL_PASSWORD"		=>	"Ihr neues Passwort lautet",
+"EMAIL_LOGIN"			=>	"Melden Sie sich hier an",
+"EMAIL_THANKYOU"		=>	"Danke f&uuml;r den Einsatz von",
+"NOTE_REGISTRATION"		=>	"Ihre Zugangsdaten wurden versendet an",
+"NOTE_REGERROR"			=>	"<b>Fehler:</b> Ihre Zugangsdaten konnten nicht per E-Mail versandt werden. Notieren Sie sich daher folgendes Passwort",
+"NOTE_USERNAME"			=>	"Ihr Benutzername lautet",
+"NOTE_PASSWORD"			=>	"Ihr Passwort lautet",
+"INSTALLATION"			=>	"Installation",
+"LABEL_WEBSITE"			=>	"Websitename",
+"LABEL_BASEURL"			=>	"Websitebasis URL",
+"LABEL_SUGGESTION"		=>	"Unser Vorschlag ist",
+"LABEL_USERNAME"		=>	"Benutzername",
+"LABEL_EMAIL"			=>	"E-Mail-Adresse",
+"LABEL_INSTALL"			=>	"Installation starten",
+"SELECT_LANGUAGE"		=> 	"W&auml;hlen Sie Ihre Sprache aus",
+"CONTINUE_SETUP" 		=> 	"mit Setup fortfahren",
+"DOWNLOAD_LANG" 		=> 	"Sprachdateien herunterladen",
+
+/* 
+ * For: pages.php
+*/
+"MENUITEM_SUBTITLE"	=>	"Men&uuml;eintrag",
+"HOMEPAGE_SUBTITLE"	=>	"Startseite",
+"PRIVATE_SUBTITLE"	=>	"Privat",
+"EDITPAGE_TITLE"	=>	"Seite bearbeiten",
+"VIEWPAGE_TITLE"	=>	"Seite anzeigen",
+"DELETEPAGE_TITLE"	=>	"Seite l&ouml;schen",
+"PAGE_MANAGEMENT"	=>	"Seitenverwaltung",
+"TOGGLE_STATUS"		=>	"Status ein-/ausblenden",
+"TOTAL_PAGES"		=>	"Seiten",
+"ALL_PAGES"		    =>	"Alle Seiten",
+
+/* 
+ * For: edit.php
+*/
+"PAGE_NOTEXIST"			=>	"Die angeforderte Seite existiert nicht",
+"BTN_SAVEPAGE"			=>	"Seite speichern",
+"BTN_SAVEUPDATES"		=>	"&Auml;nderungen speichern",
+"DEFAULT_TEMPLATE"		=>	"Standardvorlage",
+"NONE"				    =>	"Leer",
+"PAGE"				    =>	"Seite",
+"NEW_PAGE"				=>	"Neue Seite",
+"PAGE_EDIT_MODE"		=>	"Seitenbearbeitungsmodus",
+"CREATE_NEW_PAGE"		=>	"Neue Seite erstellen",
+"VIEW"					=>	"<em>V</em>orschau",
+"PAGE_OPTIONS"			=>	"<em>O</em>ptionen",
+"SLUG_URL"				=>	"Seitenname/URL",
+"TAG_KEYWORDS"			=>	"Schl&uuml;sselworte",
+"PARENT_PAGE"			=>	"&Uuml;bergeordnete Seite",
+"TEMPLATE"				=>	"Vorlage",
+"KEEP_PRIVATE"			=>	"Privatseite?",
+"ADD_TO_MENU"			=>	"Im Men&uuml;",
+"PRIORITY"				=>	"Priorit&auml;t",
+"MENU_TEXT"				=>	"Men&uuml;bezeichnung",
+"LABEL_PAGEBODY"		=>	"Seiteninhalt",
+"CANCEL"				=>	"Abbrechen",
+"BACKUP_AVAILABLE"		=>	"Sicherung verf&uuml;gbar",
+"MAX_FILE_SIZE"			=>	"Maximale Dateigr&ouml;&szlig;e",
+"LAST_SAVED"			=>	"Zuletzt gespeichert",
+"FILE_UPLOAD"			=>	"Datei hochladen",
+"OR"					=>	"oder",
+
+/* 
+ * For: upload.php
+*/
+"ERROR_UPLOAD"			=>	"Es gab ein Problem beim Hochladen der Datei",
+"FILE_SUCCESS_MSG"		=>	"Erfolg! Die Datei befindet sich unter",
+"FILE_MANAGEMENT"		=>	"Dateiverwaltung",
+"UPLOADED_FILES"		=>	"Hochgeladene Dateien",
+"SHOW_ALL"				=>	"Alle anzeigen",
+"VIEW_FILE"				=>	"Datei anzeigen",
+"DELETE_FILE"			=>	"Datei l&ouml;schen",
+"TOTAL_FILES"			=>	"Dateien",
+
+/* 
+ * For: logout.php
+*/
+"LOGGED_OUT"			=>	"Abgemeldet",
+"MSG_LOGGEDOUT"			=>	"Sie sind nun abgemeldet.",
+
+/* 
+ * For: index.php
+*/
+"LOGIN"					=>	"Login",
+"USERNAME"				=>	"Benutzername",
+"PASSWORD"				=>	"Passwort",
+"FORGOT_PWD"			=>	"Passwort vergessen?",
+"CONTROL_PANEL"			=>	"Admin-Bereich",
+
+/* 
+ * For: navigation.php
+*/
+"CURRENT_MENU" 			=> 	"Aktuelles Men&uuml;",
+"NO_MENU_PAGES" 		=> 	"Es sind keine Seiten f&uuml;r die Anzeige in der Navigation eingerichtet.",
+
+/* 
+ * For: theme-edit.php
+*/
+"TEMPLATE_FILE" 		=> 	"Die Vorlage aus der Datei <b>%s</b> wurde erfolgreich hochgeladen",
+"THEME_MANAGEMENT" 		=> 	"Vorlagenmanagement",
+"EDIT_THEME" 			=> 	"Vorlage bearbeiten",
+"EDITING_FILE" 			=> 	"Datei bearbeiten",
+"BTN_SAVECHANGES" 		=> 	"&Auml;nderungen speichern",
+"EDIT" 					=> 	"Bearbeiten",
+
+/* 
+ * For: support.php
+*/
+"SETTINGS_UPDATED"		=> 	"Ihre Einstellungen wurden aktualisiert",
+"UNDO" 					=> 	"R&uuml;ckg&auml;ngig",
+"SUPPORT" 				=> 	"Support",
+"SETTINGS" 				=> 	"Einstellungen",
+"ERROR" 				=> 	"Fehler",
+"BTN_SAVESETTINGS" 		=> 	"Einstellungen speichern",
+"VIEW_FAILED_LOGIN"		=> 	"Anzahl der fehlgeschlagenen Anmeldeversuche anzeigen",
+/* 
+ * For: log.php
+*/
+"MSG_HAS_BEEN_CLR" 		=> 	"wurde gel&ouml;scht",
+"LOGS" 					=> 	"Logs",
+"VIEWING" 				=> 	"Anzeigen",
+"LOG_FILE" 				=> 	"Log-Datei",
+"CLEAR_ALL_DATA" 		=> 	"L&ouml;sche alle Daten von",
+"CLEAR_THIS_LOG" 		=> 	"L&ouml;sche alle Logdaten",
+"LOG_FILE_ENTRY" 		=> 	"keine Eintr&auml;ge in der Log-Datei",
+"THIS_COMPUTER"			=>	"Der Computer",
+
+/* 
+ * For: backup-edit.php
+*/
+"BAK_MANAGEMENT"		=>	"Backupverwaltung",
+"ASK_CANCEL"			=>	"Abbrechen",
+"ASK_RESTORE"			=>	"Wiederherstellen",
+"ASK_DELETE"			=>	"L&ouml;schen",
+"BACKUP_OF"				=>	"Sicherung von",
+"PAGE_TITLE"			=>	"Seitentitel",
+"YES"					=>	"Ja",
+"NO"					=>	"Nein",
+"DATE"					=>	"Datum",
+
+/* 
+ * For: components.php
+*/
+"COMPONENTS"			=>	"Komponenten",
+"DELETE_COMPONENT"		=>	"Komponente l&ouml;schen",
+"EDIT"					=>	"Bearbeiten",
+"ADD_COMPONENT"			=>	"Komponente hinzuf&uuml;gen",
+"SAVE_COMPONENTS"		=>	"Komponente speichern",
+
+/* 
+ * For: sitemap.php
+*/
+"SITEMAP_CREATED"		=>	"Die Sitemap wurde erstellt! Dabei wurden auch 4 Suchmaschinen benachrichtigt",
+"SITEMAP_ERRORPING"		=>	"Die Sitemap wurde erstellt, jedoch trat ein Fehler beim Benachrichtigen der Suchmaschinen auf",
+"SITEMAP_ERROR"			=>	"Die Sitemap konnte nicht erstellt werden",
+"SITEMAP_WAIT"			=>	"<b>Bitte warten:</b> Die Sitemap wird gerade erstellt",
+
+/* 
+ * For: theme.php
+*/
+"THEME_CHANGED"			=>	"Ihre Vorlage wurde erfolgreich ge&auml;ndert",
+"CHOOSE_THEME"			=>	"W&auml;hlen Sie Ihre Vorlage aus",
+"ACTIVATE_THEME"		=>	"Vorlage aktivieren",
+"THEME_SCREENSHOT"		=>	"Screenshot der Vorlage",
+"THEME_PATH"			=>	"Pfad der aktuellen Vorlage",
+
+/* 
+ * For: resetpassword.php
+*/
+"RESET_PASSWORD"		=>	"Passwort zur&uuml;cksetzen",
+"YOUR_NEW"				=>	"Ihr neues",
+"PASSWORD_IS"			=>	"Passwort lautet",
+"ATTEMPT"				=>	"Versuch",
+"MSG_PLEASE_EMAIL"		=>	"Bitte geben Sie Ihre registrierte E-Mail - Adresse ein. Nach dem Absenden erhalten Sie eine E-Mail mit Ihrem neugenerierten Passwort",
+"SEND_NEW_PWD"			=>	"Neues Passwort anfordern",
+
+/* 
+ * For: settings.php
+*/
+"GENERAL_SETTINGS"		=>	"Allgemeine Einstellungen",
+"WEBSITE_SETTINGS"		=>	"Website-Einstellungen",
+"LOCAL_TIMEZONE"		=>	"Lokale Zeitzone",
+"LANGUAGE"				=>	"Sprache",
+"USE_FANCY_URLS"		=>	"<b>Nutze freundliche URLs</b> - Erfordert 'mod_rewrite' auf Ihrem Server",
+"ENABLE_HTML_ED"		=>	"<b>HTML Editor aktivieren</b>",
+"WARN_EMAILINVALID"		=>	"Hinweis: Die angegebene E-Mail - Adresse scheint nicht g&uuml;ltig zu sein",
+"ONLY_NEW_PASSWORD"		=>	"Wenn Sie Ihr Passwort &auml;ndern m&ouml;chten, geben Sie das neue Passwort hier ein",
+"NEW_PASSWORD"			=>	"Neues Passwort",
+"CONFIRM_PASSWORD"		=>	"Passwort wiederholen",
+"PASSWORD_NO_MATCH"		=>	"Die Passw&ouml;rter scheinen nicht &uuml;bereinzustimmen",
+"PERMALINK" 			=> 	"Custom Permalink Structure",
+"MORE" 					=> 	"Mehr Info ..",
+"HELP" 					=> 	"Hilfe",
+
+/* 
+ * For: health-check.php
+*/
+"WEB_HEALTH_CHECK"		=>	"Website-Serverpr&uuml;fung",
+"VERSION"				=>	"Version",
+"UPG_NEEDED"			=>	"Aktualisierung wird ben&ouml;tigt zu",
+"CANNOT_CHECK"			=>	"Kann nicht pr&uuml;fen. Ihre Version ist",
+"LATEST_VERSION"		=>	"Die aktuellste Version wurde installiert",
+"SERVER_SETUP"			=>	"Servereinrichtung",
+"OR_GREATER_REQ"		=>	"oder h&ouml;her wird ben&ouml;tigt",
+"OK"					=>	"OK",
+"INSTALLED"				=>	"installiert",
+"NOT_INSTALLED"			=>	"nicht installiert",
+"WARNING"				=>	"Warnung",
+"DATA_FILE_CHECK"		=>	"Test der Datendatei-Integrit&auml;t",
+"DIR_PERMISSIONS"		=>	"Verzeichnisrechte",
+"EXISTANCE"				=>	"%s vorhanden",
+"MISSING_FILE"			=>	"Fehlende Datei",
+"BAD_FILE"				=>	"Fehlerhafte Datei",
+"NO_FILE"				=>	"Keine Datei",
+"GOOD_D_FILE"			=>	"Gute 'Verbieten' Datei",
+"GOOD_A_FILE"			=>	"Gute 'Erlauben' Datei",
+"CANNOT_DEL_FILE"		=>	"Die Datei kann nicht gel&ouml;scht werden",
+"DOWNLOAD"				=>	"Download",
+"WRITABLE"				=>	"Beschreibbar",
+"NOT_WRITABLE"			=>	"Nicht beschreibbar",
+
+/* 
+ * For: footer.php
+*/
+"POWERED_BY"			=>	"Erstellt mit",
+
+/* 
+ * For: backups.php
+*/
+"PAGE_BACKUPS"			=>	"Backupverwaltung",
+"ASK_DELETE_ALL"		=>	"L&ouml;sche alle Sicherungen",
+"DELETE_ALL_BAK"		=>	"Alle Sicherungen l&ouml;schen?",
+"TOTAL_BACKUPS"			=>	"Gesamtanzahl der Sicherungen",
+
+/* 
+ * For: archive.php
+*/
+"SUCC_WEB_ARCHIVE"		=>	"Website wurde erfolgreich archiviert!",
+"SUCC_WEB_ARC_DEL"		=>	"Website-Archiv erfolgreich gel&ouml;scht",
+"WEBSITE_ARCHIVES"		=>	"Website-Archiv",
+"ARCHIVE_DELETED"		=>	"Archiv erfolgreich gel&ouml;scht",
+"CREATE_NEW_ARC"		=>	"Neues Archiv erstellen",
+"ASK_CREATE_ARC"		=>	"Neues Archiv erstellen?",
+"CREATE_ARC_WAIT"		=>	"<b>Bitte warten:</b> Website Archiv wird gerade erstellt ...",
+"DOWNLOAD_ARCHIVES"		=>	"Archiv herunterladen",
+"DELETE_ARCHIVE"		=>	"Archiv l&ouml;schen",
+"TOTAL_ARCHIVES"		=>	"Archiv&uuml;bersicht",
+
+/* 
+ * For: include-nav.php
+*/
+"WELCOME"				=>	"Willkommen",
+"TAB_PAGES"				=>	"<em>S</em>eiten",
+"TAB_FILES"				=>	"<em>D</em>ateien",
+"TAB_THEME"				=>	"<em>V</em>orlagen",
+"TAB_BACKUPS"			=>	"<em>B</em>ackups",
+"PLUGINS_NAV" 			=> 	"<em>P</em>lugins",
+"TAB_SETTINGS"			=>	"<em>E</em>instellungen",
+"TAB_SUPPORT"			=>	"Supp<em>o</em>rt",
+"TAB_LOGOUT"			=>	"<em>A</em>bmelden",
+
+/* 
+ * For: sidebar-files.php
+*/
+"BROWSE_COMPUTER"		=>	"Durchsuchen Sie Ihren Computer",
+"UPLOAD"				=>	"Hochladen",
+
+/* 
+ * For: sidebar-support.php
+*/
+"SIDE_SUPPORT_LOG"		=>	"Supp<em>o</em>rt Einstellungen &amp; Logs",
+"SIDE_VIEW_LOG"			=>	"Log anzeigen",
+"SIDE_HEALTH_CHK"		=>	"Website Serverpr&uuml;fung",
+"SIDE_DOCUMENTATION"	=>	"<em>D</em>okumentation",
+
+/* 
+ * For: sidebar-theme.php
+*/
+"SIDE_VIEW_SITEMAP"		=>	"<em>Z</em>eige Sitemap",
+"SIDE_GEN_SITEMAP"		=>	"<em>E</em>rzeuge Sitemap",
+"SIDE_COMPONENTS"		=>	"<em>B</em>earbeite Komponenten",
+"SIDE_EDIT_THEME"		=>	"Bearbeite Vorlage",
+"SIDE_CHOOSE_THEME"		=>	"Vorlage ausw&auml;hlen",
+
+/* 
+ * For: sidebar-pages.php
+*/
+"SIDE_CREATE_NEW"		=>	"<em>N</em>eue Seite erstellen",
+"SIDE_VIEW_PAGES"		=>	"Alle <em>S</em>eiten anzeigen",
+
+/* 
+ * For: sidebar-pages.php
+*/
+"SIDE_GEN_SETTINGS"		=>	"Allgemeine <em>E</em>instellungen",
+"SIDE_USER_PROFILE"		=>	"<em>B</em>enutzer Profil",
+
+/* 
+ * For: sidebar-pages.php
+*/
+"SIDE_VIEW_BAK"			=>	"Sicherung der Seite anzeigen",
+"SIDE_WEB_ARCHIVES"		=>	"<em>W</em>ebsite-Archiv",
+"SIDE_PAGE_BAK"			=>	"<em>S</em>icherungen der Website",
+
+/* 
+ * For: error_checking.php
+*/
+"ER_PWD_CHANGE"			=>	"Es wird empfohlen, das derzeit g&uuml;ltige Passwort unter <a href=\"settings.php#profile\"> abzu&auml;ndern",
+"ER_BAKUP_DELETED"		=>	"Die Sicherung wurde gel&ouml;scht f&uuml;r %s",
+"ER_REQ_PROC_FAIL"		=>	"Der Prozess ist fehlgeschlagen",
+"ER_YOUR_CHANGES"		=>	"Ihre &Auml;nderungen zu %s wurden gespeichert",
+"ER_HASBEEN_REST"		=>	"%s wurde wiederhergestellt",
+"ER_HASBEEN_DEL"		=>	"%s wurde gel&ouml;scht",
+"ER_CANNOT_INDEX"		=>	"Sie k&ouml;nnen nicht die Websitebasis URL der Startseite ver&auml;ndern",
+"ER_SETTINGS_UPD"		=>	"Ihre Einstellungen wurden gespeichert",
+"ER_OLD_RESTORED"		=>	"Ihre vorherigen Einstellungen wurden wiederhergestellt",
+"ER_NEW_PWD_SENT"		=>	"Es wurde ein neues Passwort an die angegebene E-Mail - Adresse versandt",
+"ER_SENDMAIL_ERR"		=>	"Ein Fehler beim Versenden der E-Mail ist aufgetreten. Bitte versuchen Sie es erneut",
+"ER_FILE_DEL_SUC"		=>	"Datei wurde erfolgreich gel&ouml;scht",
+"ER_PROBLEM_DEL"		=>	"Es trat ein Fehler beim L&ouml;schen der Datei auf",
+"ER_COMPONENT_SAVE"		=>	"Ihre Komponenten wurden gespeichert",
+"ER_COMPONENT_REST"		=>	"Ihre Komponenten wurden wiederhergestellt",
+"ER_CANCELLED_FAIL"		=>	"<b>Abgebrochen:</b> Die Ver&auml;nderungen wurden nicht &uuml;bernommen",
+
+/* 
+ * For: changedata.php
+*/
+"CANNOT_SAVE_EMPTY"		=>	"Sie k&ouml;nnen keine leere Seite abspeichern",
+"META_DESC" 			=>  "Meta-Beschreibung",
+
+/* 
+ * For: template_functions.php
+*/
+"FTYPE_COMPRESSED"		=>	"Komprimiert",
+"FTYPE_VECTOR"			=>	"Vector",
+"FTYPE_FLASH"			=>	"Flash",
+"FTYPE_VIDEO"			=>	"Video",
+"FTYPE_AUDIO"			=>	"Audio",
+"FTYPE_WEB"				=>	"Web",
+"FTYPE_DOCUMENTS"		=>	"Dokumente",
+"FTYPE_SYSTEM"			=>	"System",
+"FTYPE_MISC"			=>	"Sonstiges",
+"IMAGES"				=>	"Bilder",
+
+/* 
+ * For: login_functions.php
+*/
+"FILL_IN_REQ_FIELD"		=>	"Bitte f&uuml;llen Sie alle Pflichtfelder aus",
+"LOGIN_FAILED"			=>	"Anmeldung fehlgeschlagen. Bitte pr&uuml;fen Sie Ihren Benutzernamen und Passwort.",
+
+/* 
+ * For: Date Format
+*/
+"DATE_FORMAT"			=>	"d.M Y",
+"DATE_AND_TIME_FORMAT"	=>	"F jS, Y - g:i A", //Datum und Zeitangaben
+
+
+/***********************************************************************************
+ * SINCE Version 2.0 / &Auml;nderungen in Version 2.0
+***********************************************************************************/
+
+
+/* 
+ * For: welcome.php
+*/
+"WELCOME_MSG"			=>	"Vielen Dank, dass Sie sich f&uuml;r GetSimple CMS entschieden haben.",
+"WELCOME_P"				=>	"Mit GetSimple CMS verwalten Sie Ihre Website bequem online. Die einfache Handhabung der Benutzeroberfl&auml;che macht es m&ouml;glich",
+"GETTING_STARTED"		=>	"Erste Schritte",
+
+/* 
+ * For: install.php
+*/
+
+"SELECT_LANGUAGE"		=> "W&auml;hlen Sie ihre Sprache aus",
+"CONTINUE_SETUP" 		=> "Mit Installation fortfahren",
+"DOWNLOAD_LANG" 		=> "Download weiterer Sprachen",
+
+/* 
+ * For: image.php
+*/
+
+"CURRENT_THUMBNAIL" 	=> "Aktuelles Thumbnail",
+"RECREATE" 				=> "Erneut erstellen",
+"CREATE_ONE" 			=> "Erstellen",
+"IMG_CONTROl_PANEL" 	=> "Bild-Verwaltung",
+"ORIGINAL_IMG" 			=> "Originalbild",
+"CLIPBOARD_INSTR" 		=> "Alle Ausw&auml;hlen mit <em>STRG+C</em>",
+"CREATE_THUMBNAIL" 		=> "Thumbnail erstellen",
+"CROP_INSTR_NEW" 		=> "<em>ctrl-B</em> oder <em>command-B</em> für ein quadratisches Bild",
+"SELECT_DIMENTIONS" 	=> "Abmessungen ausw&auml;hlen",
+"HTML_ORIG_IMG" 		=> "Originalbild HTML",
+"LINK_ORIG_IMG" 		=> "Originalbild Link",
+"HTML_THUMBNAIL" 		=> "Thumbnail HTML",
+"LINK_THUMBNAIL" 		=> "Thumbnail Link",
+"HTML_THUMB_ORIG" 		=> "Thumbnail-zu-Bild HTML",
+
+
+/* 
+ * For: plugins.php
+*/
+
+"PLUGINS_MANAGEMENT"	=> "Pluginverwaltung",
+"PLUGINS_INSTALLED" 	=> "Installierte Plugins",
+"SHOW_PLUGINS"			=> "Installierte Plugi<em>n</em>s",
+"PLUGIN_NAME" 			=> "Name",
+"PLUGIN_DESC" 			=> "Beschreibung",
+"PLUGIN_VER" 			=> "Version",
+
+
+
+/***********************************************************************************
+ * SINCE Version 2.02
+***********************************************************************************/
+
+
+/***********************************************************************************
+ * SINCE Version 3.0
+***********************************************************************************/
+
+/* 
+ * For: setup.php
+ */
+
+"ROOT_HTACCESS_ERROR"		=> ".htacces konnte nicht im Hauptverzeichis angelegt werden. Bitte kopieren Sie <b>%s</b> als <b>.htaccess</b>  und &auml;ndern Sie <code>%s</code> in <code>%s</code> um", 
+"REMOVE_TEMPCONFIG_ERROR"	=> "<b>%s</b> konnte nicht gel&ouml;scht werden! L&ouml;schen Sie das bitte selbst.",
+"MOVE_TEMPCONFIG_ERROR" 	=> "<b>%s</b> konnte nicht in <b>%s</b> umbenannt werden! Benennen Sie bitte selbst um.",
+"KILL_CANT_CONTINUE" 		=> "Weiterarbeiten unm&ouml;glich. Bitte beheben Sie die Fehler und versuchen Sie es nocheinmal.",
+"REFRESH" 					=> "Aktualisieren",
+"BETA"						=> "Beta / Bleeding Edge",
+
+/*
+ * Misc Cleanup Work
+ */
+ 
+"HOMEPAGE_DELETE_ERROR" 	=> "Die Startseite kann nicht gel&ouml;scht werden.", //deletefile
+"NO_ZIPARCHIVE" 			=> "Die <i>ZipArchiv-Extension</i> ist nicht installiert, kann nicht fortfahren.", //zip
+"REDIRECT_MSG"				=> "Wenn Ihr Browser Sie nicht weiterleitet, klicken Sie <a href=\"%s\">hier</a>", //basic
+"REDIRECT"					=> "Weiterleitung", //basic
+"DENIED"					=> "Verweigert", //sitemap
+"DEBUG_MODE"				=> "DEBUG MODUS", //nav-include
+"DOUBLE_CLICK_EDIT"			=> "Zum Bearbeiten zweimal klicken", //components
+"THUMB_SAVED"				=> "Thumbnail wurde gespeichert", //image
+"EDIT_COMPONENTS"			=> "Komponenten bearbeiten", //components
+"REQS_MORE_INFO"			=> "Weitere Informationen zu den ben&ouml;tigten Modulen finden Sie auf <a href=\"%s\" target=\"_blank\" >requirements page</a>.", //install & health-check
+"SYSTEM_UPDATE" 			=> "System-Update", // update.php
+"AUTHOR" 					=> "Autor", //plugins.php
+"ENABLE" 					=> "Aktivieren", //plugins.php
+"DISABLE" 					=> "Deaktivieren", //plugins.php
+"NO_THEME_SCREENSHOT" 		=> "Kein Screenshot verf&uuml;gbar", //theme.php
+"UNSAVED_INFORMATION" 		=> "Wenn Sie die Seite verlassen, werden die &Auml;nderungen nicht gespeichert.", //edit.php
+"BACK_TO_WEBSITE" 			=> "Zur&uuml;ck zur Webseite", //index & resetpassword
+"SUPPORT_FORUM" 			=> "Support Forum", //support.php
+"FILTER" 					=> "Filter", //pages.php
+"UPLOADIFY_BUTTON" 			=> "Dateien und/oder Grafiken hochladen...", //upload.php
+"FILE_BROWSER" 				=> "Datei-Browser", //filebrowser.php
+"SELECT_FILE" 				=> "Datei ausw&auml;hlen", //filebrowser.php
+"CREATE_FOLDER" 			=> "Verzeichnis anlegen", //upload.php
+"THUMBNAIL" 				=> "Thumbnail", //filebrowser.php
+"ERROR_FOLDER_EXISTS" 		=> "Es gibt schon ein gleichnamiges Verzeichnis", //upload.php
+"FOLDER_CREATED" 			=> "Das neue Verzeichnis wurde angelegt: <strong>%s</strong>", //upload.php
+"ERROR_CREATING_FOLDER"		=> "Das Verzeichnis konnte nicht angelegt werden", //upload.php
+"DELETE_FOLDER"				=> "Verzeichnis l&ouml;schen", //upload.php
+"FILE_NAME" 				=> "Datei-Name", //multiple tr header rows
+"FILE_SIZE" 				=> "Gr&ouml;&szlig;e", //multiple tr header rows
+"ARCHIVE_DATE" 				=> "Archiv-Datum", //archive.php
+
+"X" => "nicht &uuml;bersetzt"
+
+);
+?>
